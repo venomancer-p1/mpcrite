a6fa7a7eb668a108c28a32d8d1c7d33434f308d1
venomancer-p1 <venomancer777@protonmail.com>
2022-06-18 19:30:34 -0400

    commit

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const express = require('express');
 const delay = require('delay');
 const axios = require('axios');
 const unirest = require('unirest')
+const recaptcha = require('./reCaptcha')
 
 var crypto = require('crypto');
 const querystring = require('querystring');
@@ -15,8 +16,8 @@ const protonmail = require('mail-proton-api');
 const cheerio = require('cheerio');
 const useProxy = require('puppeteer-page-proxy');
 var randomWords = require('random-words');
-//const name_list = fs.readFileSync(path.join(__dirname, 'name_list.txt')).toString().replace(/\r\n/g, '\n').split('\n');
-//const proxies = fs.readFileSync(path.join(__dirname, 'proxies.txt')).toString().replace(/\r\n/g, '\n').split('\n');
+const name_list = fs.readFileSync(path.join(__dirname, 'name_list.txt')).toString().replace(/\r\n/g, '\n').split('\n');
+const proxies = fs.readFileSync(path.join(__dirname, 'proxies.txt')).toString().replace(/\r\n/g, '\n').split('\n');
 var UA = require('user-agents');
 var httpsProxyAgent = require('https-proxy-agent');
 
@@ -293,196 +294,8 @@ app.get('/p/mail', async (req, res) => {
   return res.end();
 
 })
+
 app.get('/p/create', async (req, res) => {
-
-
-  let headers = {
-    "Host": "hcaptcha.com",
-    "Connection": "keep-alive",
-    "sec-ch-ua": 'Chromium";v="92", " Not A;Brand";v="99", "Google Chrome";v="92',
-    "Accept": "application/json",
-    "sec-ch-ua-mobile": "?0",
-    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36",
-    "Content-type": "application/json; charset=utf-8",
-    "Origin": "https://newassets.hcaptcha.com",
-    "Sec-Fetch-Site": "same-site",
-    "Sec-Fetch-Mode": "cors",
-    "Sec-Fetch-Dest": "empty",
-    "Referer": "https://newassets.hcaptcha.com/",
-    "Accept-Language": "en-US,en;q=0.9"
-  }
-
-  function N_Data(requ) {
-    let x = "0123456789/:abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
-    requ = requ.split(".")
-
-    requ = {
-      "header": JSON.parse(Buffer.from(requ[0] + "=======", 'base64').toString('utf-8')),
-      "payload": JSON.parse(Buffer.from(requ[1] + "=======", 'base64').toString('utf-8')),
-      "raw": {
-        "header": requ[0],
-        "payload": requ[1],
-        "signature": requ[2]
-      }
-    }
-
-    function a(r) {
-      for (let t = r.length - 1; t >= 0; t--) {
-        //console.log(arr[i]);
-        if (r[t] < (x.length - 1)) {
-          r[t] += 1
-          return true
-        }
-        r[t] = 0
-      }
-      return false
-    }
-    function i(r) {
-      t = ""
-      for (let n = 0; n < r.length; n++) {
-        t += x[r[n]]
-      }
-      return t
-    }
-    function o(r, e) {
-      var a, hashed, n, o, t;
-      n = e;
-      //crypto.createHash('sha1')
-      hashed = crypto.createHash('sha1').update(Buffer.from(e, 'utf-8'));
-      hashed2 = crypto.createHash('sha1').update(Buffer.from(e, 'utf-8'));
-      //console.log(Buffer.from(e, 'utf-8'))
-      o = hashed.digest('hex');
-      t = hashed2.digest();
-      //console.log(t)
-      /*
-      hashed = hashlib.sha1(e.encode());
-      o = hashed.hexdigest();
-      t = hashed.digest();*/
-      e = null;
-      n = -1;
-      o = [];
-
-      for (n = n + 1; n < (8 * t.length); n++) {
-        e = t[Math.floor(n / 8)] >> n % 8 & 1;
-        o.push(e);
-      }
-
-      a = o.slice(0, r);
-      function index2(x, y) {
-        if (x.includes(y)) {
-          return x.indexOf(y)
-        }
-        return -1;
-      }
-      //console.log((0 === a[0]) && (index2(a, 1) >= (r - 1)) || (-1 === index2(a, 1)))
-      return (0 == a[0]) && (index2(a, 1) >= (r - 1)) || (-1 == index2(a, 1));
-    }
-
-    function get() {
-      for (let e = 0; e < 25; e++) {
-        n = Array.from({ length: e }, (_, i) => 0)
-        while (a(n)) {
-          u = requ["payload"]["d"] + "::" + i(n)
-          if (o(requ["payload"]["s"], u)) {
-            return i(n)
-          }
-        }
-      }
-    }
-
-    let result = get();
-    hsl = [
-      "1",
-      requ["payload"]["s"].toString(),
-      new Date().toISOString().slice(0, 19).replaceAll("T", "").replaceAll("-", "").replaceAll(":", ""),
-      requ["payload"]["d"],
-      "",
-      result
-    ].join(':')
-
-    return hsl;
-  }
-  async function REQ_Data(host, sitekey) {
-    try {
-      let r = await axios.get(`https://hcaptcha.com/checksiteconfig?host=${host}&sitekey=${sitekey}&sc=1&swa=1`, { headers: headers })
-      r = r.data
-      if (r["pass"])
-        return r["c"];
-      else
-        return false;
-    } catch (error) {
-      return false;
-    }
-  }
-  async function Get_Captcha(host, sitekey, n, requ/*, proxy*/) {
-
-    try {
-
-
-      json = {
-        "sitekey": sitekey,
-        "v": "b1129b9",
-        "host": host,
-        "n": n,
-        'motiondata': '{"st":1628923867722,"mm":[[203,16,1628923874730],[155,42,1628923874753],[137,53,1628923874770],[122,62,1628923874793],[120,62,1628923875020],[107,62,1628923875042],[100,61,1628923875058],[93,60,1628923875074],[89,59,1628923875090],[88,59,1628923875106],[87,59,1628923875131],[87,59,1628923875155],[84,56,1628923875171],[76,51,1628923875187],[70,47,1628923875203],[65,44,1628923875219],[63,42,1628923875235],[62,41,1628923875251],[61,41,1628923875307],[58,39,1628923875324],[54,38,1628923875340],[49,36,1628923875363],[44,36,1628923875380],[41,35,1628923875396],[40,35,1628923875412],[38,35,1628923875428],[38,35,1628923875444],[37,35,1628923875460],[37,35,1628923875476],[37,35,1628923875492]],"mm-mp":13.05084745762712,"md":[[37,35,1628923875529]],"md-mp":0,"mu":[[37,35,1628923875586]],"mu-mp":0,"v":1,"topLevel":{"st":1628923867123,"sc":{"availWidth":1680,"availHeight":932,"width":1680,"height":1050,"colorDepth":30,"pixelDepth":30,"availLeft":0,"availTop":23},"nv":{"vendorSub":"","productSub":"20030107","vendor":"Google Inc.","maxTouchPoints":0,"userActivation":{},"doNotTrack":null,"geolocation":{},"connection":{},"webkitTemporaryStorage":{},"webkitPersistentStorage":{},"hardwareConcurrency":12,"cookieEnabled":true,"appCodeName":"Mozilla","appName":"Netscape","appVersion":"5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36","platform":"MacIntel","product":"Gecko","userAgent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36","language":"en-US","languages":["en-US","en"],"onLine":true,"webdriver":false,"serial":{},"scheduling":{},"xr":{},"mediaCapabilities":{},"permissions":{},"locks":{},"usb":{},"mediaSession":{},"clipboard":{},"credentials":{},"keyboard":{},"mediaDevices":{},"storage":{},"serviceWorker":{},"wakeLock":{},"deviceMemory":8,"hid":{},"presentation":{},"userAgentData":{},"bluetooth":{},"managed":{},"plugins":["internal-pdf-viewer","mhjfbmdgcfjbbpaeojofohoefgiehjai","internal-nacl-plugin"]},"dr":"https://discord.com/","inv":false,"exec":false,"wn":[[1463,731,2,1628923867124],[733,731,2,1628923871704]],"wn-mp":4580,"xy":[[0,0,1,1628923867125]],"xy-mp":0,"mm":[[1108,233,1628923867644],[1110,230,1628923867660],[1125,212,1628923867678],[1140,195,1628923867694],[1158,173,1628923867711],[1179,152,1628923867727],[1199,133,1628923867744],[1221,114,1628923867768],[1257,90,1628923867795],[1272,82,1628923867811],[1287,76,1628923867827],[1299,71,1628923867844],[1309,68,1628923867861],[1315,66,1628923867877],[1326,64,1628923867894],[1331,62,1628923867911],[1336,60,1628923867927],[1339,58,1628923867944],[1343,56,1628923867961],[1345,54,1628923867978],[1347,53,1628923867994],[1348,52,1628923868011],[1350,51,1628923868028],[1354,49,1628923868045],[1366,44,1628923868077],[1374,41,1628923868094],[1388,36,1628923868110],[1399,31,1628923868127],[1413,25,1628923868144],[1424,18,1628923868161],[1436,10,1628923868178],[1445,3,1628923868195],[995,502,1628923871369],[722,324,1628923874673],[625,356,1628923874689],[523,397,1628923874705],[457,425,1628923874721]],"mm-mp":164.7674418604651},"session":[],"widgetList":["0a1l5c3yudk4"],"widgetId":"0a1l5c3yudk4","href":"https://discord.com/register","prev":{"escaped":false,"passed":false,"expiredChallenge":false,"expiredResponse":false}}',
-        "hl": "en",
-        "c": JSON.stringify(requ)
-      }
-      data = querystring.stringify(json);
-      headers_ = {
-        "Host": "hcaptcha.com",
-        "Connection": "keep-alive",
-        "sec-ch-ua": 'Chromium";v="92", " Not A;Brand";v="99", "Google Chrome";v="92',
-        "Accept": "application/json",
-        "sec-ch-ua-mobile": "?0",
-        "Content-length": data.length.toString(),
-        "Cookie": "hc_accessibility=M3aoUjgk2I5GqkNLVZpoiicU10DzroU0ke89nx1HHYGdRN2IDEFh3IMmXWa7lTp+tM0ZB2CSZn8wWxUvnWywRNd3w4fo4WRlc11MCy4Z/aQcyTKdj+TOMPvZZfZf7gusIAUv2L0mPjVFGL49zMk75pA+wpiwAaXePa98MULN0NN5fES1thuKHFQq64P3pIocrDiygT//N5hVvZUbon5eocSuncm1JYlNYs0DMFJtQlvZA0+9jh+/nPHrlXl/90UZ7uhg3WWfbeieHFBvfmkRzep0YBviXv5Ct+nW5q3biKJq6i/nxj28Z6Civ5dVglXt5HM8p7s8iOwntYEeHYUAuUBmbWfpvWAlQgGGZIaczZEaPk+wTvq1Q9ZNVPKYKW1l5clYSktQMEbxpCHIrBi+qDNseokuzt4g9NsHjr8XMBpdw8f2xKiNfxLxnBRVoGMZSGnCChJbQqagixvWbnjThljIAifpJldOSxLNc8DuYPqvKEI+8GrJBQnP/nOKY+hyhJfN/jUqP/frPq4abETSE5hWr8BHw5by91ApQKLZSzt3quB2lYBNMewpkDK6gr07PTR13Bv5+H+oX6sREzL2pvy2NmRsz7/vwT2/wbmfIlR2xcCYTKrwZmNhhjR/yCgxM8pqt94iAPeie/ND62S+n5usNfotQUbH8/vchPaegvGy5FzspzTC2+I1tyeS1mMMNVNZK80bbhOZIGkGjOzyLQwlFZv8vPtHgyc3qbyo0spCCnIKfP9XzisFddwHMXEAl+6+Ka2XgDt93hw5rq82pMXJP/LbMZGzH0dKz5WWC2Bnu4875quFhibp8czuv/ugI80TqMjDOtVnKn+r0QVonlW5McN0KjZuVuEOFpxfVFFjDEw0navEK4HpqPUiqiame7y4RIkEonBXPdW3EljD0mIG3FUTiwujyKPJl+nilV0BsaBACf2mOGRKHkdLo1KSEND8ujdtAQlO5NZCuQ6gQjmfJ8rLDwQNuqLBDHzHCphmkU6pUhszJCuOz9J8IMpR0f4fOzGFd7gPf50nvER0uPjf9h2qMm+zVLLv+h6ktlaI3MZVbZXxLEp5NxhU2IF4WvztxrrGbMLrLHsA5QgoYA==pl8YHSFx58hJrlCa;",
-        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36",
-        "Content-type": "application/x-www-form-urlencoded",
-        "Origin": "https://newassets.hcaptcha.com",
-        "Sec-Fetch-Site": "same-site",
-        "Sec-Fetch-Mode": "cors",
-        "Sec-Fetch-Dest": "empty",
-        "Referer": "https://newassets.hcaptcha.com/",
-        "Accept-Language": "en-US,en;q=0.9"
-      }
-
-
-      r = await axios.post(`https://hcaptcha.com/getcaptcha?s=${sitekey}`, data, { headers: headers_ })
-
-      return r.data
-      /*
-      function myPromise(timeout) {
-        return new Promise(async (resolve, reject) => {
-          // Set up the timeout
-          const timer = setTimeout(() => {
-            console.log('timedout')
-            resolve(null);
-          }, timeout);
-          try {
-            r = await axios.post(`https://hcaptcha.com/getcaptcha?s=${sitekey}`, data, {
-              headers: headers_,
-              proxy: false,
-              httpsAgent: new httpsProxyAgent.HttpsProxyAgent('http://' + proxy)
-            })
-            resolve(r.data)
-            //clearTimeout(timer);
-          } catch (error) {
-            resolve(null)
-          } finally {
-            clearTimeout(timer)
-          }
-
-        });
-      }
-      let ret = await myPromise(4000);
-      return ret;*/
-
-    } catch (error) {
-      console.log(error)
-    }
-
-  }
 
   res.writeHead(202, { 'Content-Type': 'application/json' });
   if (!req.query.email || !req.query.pass) {
@@ -560,8 +373,9 @@ app.get('/p/create', async (req, res) => {
           let data_ = request.postData();
           let headers_ = request.headers();
           request.abort()
-
-          var resp = await unirest.post(url_).proxy('http://scrapingdog:62ae1ad7327c777636c634e8-country=random@proxy.scrapingdog.com:8081').headers(headers_).send(JSON.parse(data_))
+          //62ae4f79883d62763d27004f
+          //62ae5651883d62763d270050
+          var resp = await unirest.post(url_).proxy(`http://scrapingdog:${proxies[Math.floor(Math.random() * proxies.length)]}-country=random@proxy.scrapingdog.com:8081`).headers(headers_).send(JSON.parse(data_))
           console.log(resp.body)
           /*var headers = headers_;
           var data = JSON.parse(data_);
@@ -988,20 +802,19 @@ app.get('/p/cookie', async (req, res) => {
 })
 
 
-app.get('/p/tor', async (req, res) => {
+app.get('/p/dog', async (req, res) => {
 
-  if (!req.query.server) {
-    res.set('Content-Type', 'text/html');
-    return res.status(404).send('<h3>Not Found<h3>')
-  }
-  console.log(req.query.server)
-  res.writeHead(202, { 'Content-Type': 'text/html' });
-  //127.0.0.1:9052
-  const extension = path.join(__dirname, 'callbackHooker')
+  /*
+    if (!req.query.email || !req.query.pass) {
+      res.set('Content-Type', 'text/html');
+      return res.status(404).send('<h3>Not Found<h3>')
+    }*/
+
+  res.writeHead(202, { 'Content-Type': 'application/json' });
+  const extension = path.join(__dirname, '1.3.1_1')
   const browser = await puppeteerS.launch({
     headless: true,
     args: [
-      '--proxy-server=socks5://127.0.0.1:9052',
       `--headless=chrome`,
       `--disable-extensions-except=${extension}`,
       `--load-extension=${extension}`,
@@ -1010,6 +823,7 @@ app.get('/p/tor', async (req, res) => {
     ignoreDefaultArgs: ["--enable-automation"],//  ./myUserDataDir
     userDataDir: './myUserDataDir'//MUDARRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRR <-------------------------------------------------------------------------mudar no deploy
   })
+
   console.log('Init');
   res.setTimeout(150000, function () {
     console.log('Request has timed out.');
@@ -1017,34 +831,122 @@ app.get('/p/tor', async (req, res) => {
     res.sendStatus(408);
   });
   req.on('close', () => {
-    console.log('browser closed')
+    console.log('browser closed1')
     browser.close()
     return res.end();
   });
   req.on('end', () => {
-    console.log('browser closed');
+    console.log('browser closed2');
     browser.close()
     return res.end();
   });
 
   try {
 
-
     const context = await browser.createIncognitoBrowserContext();
     const page = await context.newPage();
     const userAgent = new UA();
     await page.setUserAgent(userAgent.toString())
-    await page.goto('https://api.ipify.org');
-    await delay(5000)
-    const base64 = await page.screenshot({ encoding: "base64" });
-    res.write(`<img src="data:image/png;base64,${base64}"></img><br>`);
-    res.end();
+    await page.setRequestInterception(true);
+    var index = 0;
+    //let alive = await axios.get(`https://entrevidato.herokuapp.com/get`);
+    //let d = alive.data;
+    var api_k = ''
+    page.on('request', async request => {
+      if (request.url().includes('/register') && request.method() === "POST") {
+
+        console.log('Intercepted');
+        let url_ = request.url();
+        let data_ = request.postData();
+        let headers_ = request.headers();
+        request.abort()
+        //62ae4f79883d62763d27004f
+        //62ae5651883d62763d270050
+        //62ae5984fd72c2764482cfea
+        //62ae5bb0327c777636c634eb
+        //62ae5c2d883d62763d270052
+        //62ae5fb211d1ea764b2f264c
+        //62ae5f6f883d62763d270054
+        /*var resp = await */
+        unirest.post(url_).proxy(`http://scrapingdog:${proxies[Math.floor(Math.random() * proxies.length)]}-country=random@proxy.scrapingdog.com:8081`).headers(headers_).send(JSON.parse(data_)).then((response) => {
+          console.log(response.body)
+          api_k = response.body._id;
+        })
+
+
+
+      }
+      else {
+        request.continue();
+      }
+
+    });
+    await page.goto('https://api.scrapingdog.com/register', { timeout: 25000, waitUntil: 'networkidle0' });
+
+
+
+    //--------------------------------------EMAIL
+    task = randomIntFromInterval(0, 6)
+    sid = randomIntFromInterval(100000, 999999)
+    new_tempmail = await axios.post(`https://api.mytemp.email/1/inbox/create?sid=${sid}&task=${task}&tt=138`);
+    hash = new_tempmail.data.hash;
+    mail = new_tempmail.data.inbox;
+    console.log(new_tempmail.data.inbox)
+    //--------------------------------------
+
+    await delay(3000)
+    let random_1 = name_list[Math.floor(Math.random() * name_list.length)] + randomWords({ exactly: 2, join: '' });
+    let random_2 = name_list[Math.floor(Math.random() * name_list.length)] + randomWords({ exactly: 3, join: '' });
+    await page.type('#defaultFormLoginEmailEx', random_1, { delay: 10 });
+    await page.type('input[name="email"]', mail, { delay: 10 });
+    await page.type('input[name="password"]', random_2, { delay: 10 });
+    console.log(await recaptcha(page)); //Solve recaptcha on page;
+    await delay(2000)
+    await page.click('button[type="submit"]', { button: 'left' })
+
+    let seconds = 0;
+    let checka = setInterval(async function () {
+
+      //--------------------------------------EMAIL2
+      new_tempmail = await axios.get(`https://api.mytemp.email/1/inbox/check?inbox=${mail}&hash=${hash}&sid=${sid}&task=${task}&tt=138`);
+      if (new_tempmail.data.emls[0].from_name === 'Scrapingdog') {
+        clearInterval(checka)
+        eml = new_tempmail.data.emls[0].eml;
+        hash2 = new_tempmail.data.emls[0].hash;
+        tempmail_text = await axios.get(`https://api.mytemp.email/1/eml/get?eml=${eml}&hash=${hash2}&sid=${sid}&task=${task}&tt=429`);
+        body = tempmail_text.data.body_html
+        confimation_link = body.match(/https\:\/\/api\.scrapingdog\.com\/verify\/[^\<\/]*/g)
+        console.log(confimation_link[0])
+
+        await page.goto(confimation_link[0], { timeout: 25000, waitUntil: 'networkidle0' });
+        await delay(2000)
+
+        console.log('DONE!!!')
+        res.write(`{"status": "success", "api_key":"${api_k}"}`);
+        res.end();
+        browser.close()
+      }
+      //--------------------------------------
+
+
+
+      seconds++;
+
+      if (seconds > 25) {
+        clearInterval(checka)
+        throw new Error('Timeout during resolve')
+      }
+
+    }, 1000)
+
+    //const base64 = await page.screenshot({ encoding: "base64" });
+    //res.write(`<img src="data:image/png;base64,${base64}"></img><br>`);
+    //res.end();
+
   } catch (error) {
     console.log(error)
     res.write(`{"status": "failed", "reason":"Internal Error"}`);
     res.end();
-  } finally {
-    console.log('browser closed')
     browser.close()
   }
 
---
+++ reCaptcha.js
@@ -0,0 +1,93 @@
+const delay = require('delay');
+
+async function isVisible(page, element) {
+    return new Promise(async (resolve, reject) => {
+        let isVis = await page.evaluate((element) => {
+            const e = document.querySelector(element);
+            if (!e)
+                return false;
+            const style = e.style;
+            return style && style.display !== 'none' && style.visibility !== 'hidden' && style.opacity !== '0';
+        }, element);
+        resolve(isVis);
+    });
+}
+async function handler(page, frame, content_frame) {
+    return new Promise(async (resolve, reject) => {
+        let seconds = 0;
+        let check = setInterval(async function () {
+            //.rc-anchor-error-msg-container
+            // console.log('anchor-error', await isVisible(frame, '.rc-anchor-error-msg-container'));
+            // console.log('content-frame-error', await isVisible(content_frame, '.rc-doscaptcha-body-text'));
+
+
+            if (await isVisible(content_frame, '.rc-doscaptcha-body-text')) {
+                console.log('error');
+                //await delay(1500);
+                clearInterval(check)
+                const error_text = await content_frame.$eval('.rc-doscaptcha-body-text', element => element.innerText);
+                if (error_text.includes('Your computer or network may be sending automated queries')) reject(new Error('Could not resolve captcha'))
+            }
+            else if (await isVisible(content_frame, '.rc-defaultchallenge-incorrect-response')) {
+                await content_frame.click('#recaptcha-reload-button', { button: 'left' });
+            }
+            else if (await frame.$('#recaptcha-anchor[aria-checked*="true"]')) {
+                console.log('solved');
+                clearInterval(check);
+                resolve(true);
+            }
+            else if (await content_frame.$('.help-button-holder')) {
+                console.log('solving');
+                //await delay(1500);
+                await content_frame.click('.help-button-holder', { button: 'left' });
+            }
+            else {
+                console.log('not Found');
+            }
+
+            seconds++;
+
+            if (seconds > 15) {
+                clearInterval(check)
+                reject(new Error('Timeout during resolve'))
+            }
+
+        }, 1000)
+
+    });
+}
+
+async function solve_recaptcha(page) {
+    return new Promise(async (resolve, reject) => {
+
+        try {
+
+            await page.waitForSelector('iframe[src*="https://www.google.com/recaptcha/api2/anchor"]', { visible: true, timeout: 30000 });
+            const frames = await page.frames();
+            const frame = frames.find(frame => frame.url().includes('/recaptcha/api2/anchor?'));
+            const content_frame = frames.find(frame => frame.url().includes('/recaptcha/api2/bframe?'));
+
+            await frame.waitForSelector('#recaptcha-anchor', { visible: true, timeout: 15000 });
+            await delay(1500);
+            await frame.click('#recaptcha-anchor', { button: 'left' });
+
+            await handler(page, frame, content_frame);
+
+            /*await content_frame.waitForSelector('.help-button-holder', { visible: true, timeout: 25000 });
+            await delay(1500);
+            await content_frame.click('.help-button-holder', { button: 'left' });
+
+            console.log('[RECAPTCHA] Solve Button Clicked');
+
+            await frame.waitForSelector('#recaptcha-anchor[aria-checked*="true"]', { timeout: 25000, visible: true })*/
+
+            resolve(true)
+
+        } catch (error) {
+            reject(error)
+        }
+
+    });
+}
+
+module.exports = solve_recaptcha;
